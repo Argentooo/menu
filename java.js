e3006de4e3e2ca96e199c2b7072dce5754aa817a
Argen Saizhanov <argen.saipzhanov.alt@gmail.com>
2022-07-07 19:13:28 +0300

    Add Menu Toggle

--- java.js
+++ java.js
@@ -1,7 +1,6 @@
 const button = document.querySelector(".button");
 const mobile = document.querySelector(".mobile");
 const mobile2 = document.querySelector(".mobile2");
-console.log(mobile, mobile2);
 
 function showMenu() {
   mobile2.classList.toggle("mobile");
